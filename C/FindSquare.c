78137bc9e4b18e2a905195829d54d655fd06751b
sagsango <34793802+sagsango@users.noreply.github.com>
2019-10-26 17:29:35 +0530

    Create FindSquare.c

---
+++ C/FindSquare.c
@@ -0,0 +1,14 @@
+/**
+ * geiven an intiger n ( n>=0 & n<=1e9)
+ * find its square 
+ **/
+ 
+#include<stdio.h>
+int main()
+{
+	long long int n;
+	scanf("%lld",&n);
+	printf("sqaure of %lld is:%lld\n",n,n*n);
+	return 0;
+}
+	
